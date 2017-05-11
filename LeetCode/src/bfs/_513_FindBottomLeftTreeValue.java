a85da2cd4cae6892de9eacbb66757bcb97d62ea8
turingfly <chengfei@udel.edu>
2017-05-11 10:39:09 -0400

    bfs

---
+++ LeetCode/src/bfs/_513_FindBottomLeftTreeValue.java
@@ -0,0 +1,40 @@
+package bfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 
+ * Problem: Given a binary tree, find the leftmost value in the last row of the
+ * tree.
+ * 
+ * Time Complexity:
+ * 
+ * Space Complexity:
+ */
+public class _513_FindBottomLeftTreeValue {
+	public int findBottomLeftValue(TreeNode root) {
+		if (root == null) {
+			return -1;
+		}
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.offer(root);
+		int res = 0;
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			res = queue.peek().val;
+			// remove all the node in the current row and 
+			//add all the children in the next row
+			for (int i = 0; i < size; i++) {
+				TreeNode tmp = queue.poll();
+				if (tmp.left != null) {
+					queue.offer(tmp.left);
+				}
+				if (tmp.right != null) {
+					queue.offer(tmp.right);
+				}
+			}
+		}
+		return res;
+	}
+}
