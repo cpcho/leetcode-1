9476fcf12f2555b008fbc25bddafebf13992f6bd
turingfly <chengfei@udel.edu>
2017-08-23 18:58:04 -0400

    DP

---
+++ LeetCode/src/dynamicProgramming/_264_UglyNumberII.java
@@ -0,0 +1,38 @@
+package dynamicProgramming;
+
+/**
+ * 
+ * Problem: Write a program to find the n-th ugly number.
+ * 
+ * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+ * For example, 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the first 10
+ * ugly numbers.
+ * 
+ * Note that 1 is typically treated as an ugly number, and n does not exceed
+ * 1690.
+ * 
+ * Solution:     
+ * (1) 1×2, 2×2, 3×2, 4×2, 5×2, …
+ * (2) 1×3, 2×3, 3×3, 4×3, 5×3, …
+ * (3) 1×5, 2×5, 3×5, 4×5, 5×5, …
+ *
+ */
+public class _264_UglyNumberII {
+	public int nthUglyNumber(int n) {
+		int[] res = new int[n];
+		res[0] = 1;
+		int[] factor = { 2, 3, 5 };
+		int[] factors = { 2, 3, 5 };
+		int[] indexes = { 0, 0, 0 };
+		for (int i = 1; i < n; i++) {
+			int min = Math.min(factors[0], Math.min(factors[1], factors[2]));
+			res[i] = min;
+			for (int j = 0; j < 3; j++) {
+				if (factors[j] == min) {
+					factors[j] = res[++indexes[j]] * factor[j];
+				}
+			}
+		}
+		return res[n - 1];
+	}
+}
---
+++ LeetCode/src/dynamicProgramming/_276_PaintFence.java
@@ -0,0 +1,35 @@
+package dynamicProgramming;
+
+/**
+ * 
+ * Problem: There is a fence with n posts, each post can be painted with one of
+ * the k colors.
+ * 
+ * You have to paint all the posts such that no more than two adjacent fence
+ * posts have the same color.
+ * 
+ * Return the total number of ways you can paint the fence.
+ * 
+ * Note: n and k are non-negative integers.
+ *
+ */
+public class _276_PaintFence {
+	public class Solution {
+		public int numWays(int n, int k) {
+			if (n == 0) {
+				return 0;
+			}
+			if (n == 1) {
+				return k;
+			}
+			int diffColor = k * (k - 1);
+			int sameColor = k;
+			for (int i = 2; i < n; i++) {
+				int tmp = diffColor;
+				diffColor = (diffColor + sameColor) * (k - 1);
+				sameColor = tmp;
+			}
+			return diffColor + sameColor;
+		}
+	}
+}
