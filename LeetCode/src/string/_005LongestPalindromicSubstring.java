5ab066a4c1a799f74b0a980aebf0c7be306581a3
turingfly <chengfei@udel.edu>
2017-08-27 22:20:30 -0400

    String

---
+++ LeetCode/src/string/_005LongestPalindromicSubstring.java
@@ -0,0 +1,46 @@
+package string;
+
+/**
+ * 
+ * Problem: Given a string s, find the longest palindromic substring in s. You
+ * may assume that the maximum length of s is 1000.
+ * 
+ * Example:
+ * 
+ * Input: "babad"
+ * 
+ * Output: "bab"
+ * 
+ * Note: "aba" is also a valid answer. Example:
+ * 
+ * Input: "cbbd"
+ * 
+ * Output: "bb"
+ *
+ */
+public class _005LongestPalindromicSubstring {
+	public class Solution {
+		public String longestPalindrome(String s) {
+			int left = 0;
+			int right = 0;
+			for (int i = 0; i < s.length(); i++) {
+				int len1 = expandAroundCenter(s, i, i); // odd
+				int len2 = expandAroundCenter(s, i, i + 1); // even
+				int len = Math.max(len1, len2);
+				if (len > right - left) {
+					left = i - (len - 1) / 2;
+					right = i + len / 2;
+				}
+			}
+			return s.substring(left, right + 1);
+		}
+
+		private int expandAroundCenter(String s, int left, int right) {
+			while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+				left--;
+				right++;
+			}
+			return right - left - 1;
+		}
+	}
+}
---
+++ LeetCode/src/string/_006_ZigZagConversion.java
@@ -0,0 +1,46 @@
+package string;
+
+/**
+ * 
+ * Problem: The string "PAYPALISHIRING" is written in a zigzag pattern on a
+ * given number of rows like this: (you may want to display this pattern in a
+ * fixed font for better legibility)
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * 
+ * And then read line by line: "PAHNAPLSIIGYIR" Write the code that will take a
+ * string and make this conversion given a number of rows:
+ * 
+ * string convert(string text, int nRows); convert("PAYPALISHIRING", 3) should
+ * return "PAHNAPLSIIGYIR".
+ * 
+ */
+public class _006_ZigZagConversion {
+	public String convert(String s, int numRows) {
+		if (s == null || s.length() == 0 || numRows <= 0) {
+			return "";
+		}
+		char[] c = s.toCharArray();
+		int len = c.length;
+		StringBuilder[] sbs = new StringBuilder[numRows];
+		for (int i = 0; i < sbs.length; i++) {
+			sbs[i] = new StringBuilder();
+		}
+		int i = 0;
+		while (i < len) {
+			// Vertically Down
+			for (int j = 0; j < numRows && i < len; j++) {
+				sbs[j].append(c[i++]);
+			}
+			// obliquely up
+			for (int j = numRows - 2; j >= 1 && i < len; j--) {
+				sbs[j].append(c[i++]);
+			}
+		}
+		for (int j = 1; j < sbs.length; j++) {
+			sbs[0].append(sbs[j]);
+		}
+		return sbs[0].toString();
+	}
+}
