4bc4bc6ea7f20879e087db8b9ea13de828f81dd9
turingfly <chengfei@udel.edu>
2017-11-29 19:05:46 -0500

    Queue

--- LeetCode/src/queue/_239_SlidingWindowMaximum.java
+++ LeetCode/src/queue/_239_SlidingWindowMaximum.java
@@ -5,10 +5,11 @@ import java.util.Deque;
 
 /**
  * 
- * Problem: Given an array nums, there is a sliding window of size k which is
- * moving from the very left of the array to the very right. You can only see
- * the k numbers in the window. Each time the sliding window moves right by one
- * position.
+ * Problem:
+ * 
+ * Given an array nums, there is a sliding window of size k which is moving from
+ * the very left of the array to the very right. You can only see the k numbers
+ * in the window. Each time the sliding window moves right by one position.
  * 
  * For example, Given nums = [1,3,-1,-3,5,3,6,7], and k = 3.
  * 
